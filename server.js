cfbe8ce0f0f3dcc7b3c17a0d56c14ff0cb1f3678
jaume768 <jaumefernandezsunyer10@gmail.com>
2025-02-19 19:39:59 +0100

    implementat cors

--- server.js
+++ server.js
@@ -38,6 +38,14 @@ app.use((err, req, res, next) => {
   res.status(500).json({ error: err.message });
 });
 
+const allowedOrigin = 'https://frontend-student-station-production.up.railway.app';
+
+app.use(cors({
+  origin: allowedOrigin,
+  credentials: true, // Permite que se envíen cookies y cabeceras de autorización
+}));
+
+
 // Iniciar el servidor
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Servidor corriendo en el puerto ${PORT}`));
